fb9aace60816308179262b2120f5d3b6f69c1860
John Bolos <j_bolos@logicbaseinteractive.com>
2022-10-11 17:15:47 +0800

    add post type as PHP file

--- functions.php
+++ functions.php
@@ -27,7 +27,8 @@ $understrap_includes = array(
 	'/editor.php',                          // Load Editor functions.
 	'/block-editor.php',                    // Load Block Editor functions.
 	'/deprecated.php',                      // Load deprecated functions.
-	'/acf-fields.php'
+	'/acf-fields.php',
+	'/post-types.php'
 );
 
 // Load WooCommerce functions if WooCommerce is activated.
