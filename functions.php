ff9ce4e9d032404e3d7507e41e8b4fb2d87a78b2
John Bolos <j_bolos@logicbaseinteractive.com>
2022-10-11 16:56:21 +0800

    add ACF via PHP

--- functions.php
+++ functions.php
@@ -27,6 +27,7 @@ $understrap_includes = array(
 	'/editor.php',                          // Load Editor functions.
 	'/block-editor.php',                    // Load Block Editor functions.
 	'/deprecated.php',                      // Load deprecated functions.
+	'/acf-fields.php'
 );
 
 // Load WooCommerce functions if WooCommerce is activated.
